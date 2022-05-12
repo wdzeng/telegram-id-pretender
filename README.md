5d5387f0c6518b3a3abfdf1a2c171d2e4486915f
Hyperbola <me@hyperbola.me>
2022-05-13 01:11:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,59 @@
+# Telegram Peeker
+
+Peek at other's Telegram username, taking it over once it is released.
+
+## Install
+
+The [telethon](https://docs.telethon.dev/en/stable/) library is used. Python 3 is required.
+
+```python
+pip3 install telethon
+```
+
+## Usage
+
+The script should be used with cron. A running is an attempt to change the username.
+
+Following command tries to update username to `fzhong`.
+
+```py
+TG_API_ID=<api_id> TG_API_HASH=<api_hash> DESIRED_USERNAME=fzhong python3 main.py
+```
+
+### Login Automatically
+
+The script asks your login interactively. To login automatically, you can provided a file to save and restore the session:
+
+```py
+TG_API_ID=<api_id> TG_API_HASH=<api_hash> DESIRED_USERNAME=fzhong TG_SESSION_PATH=~/session python3 main.py
+```
+
+If such file is specified, the script tries to read session token from the file and uses that token to login. If such file does not exist, or if the session token is invalid, the script prompts your login and then save the updated token to that file. Keep that file secret since it contains sensitive data.
+
+If the script is not running non-interactively, the session file should be specified. Otherwise the script exits without asking your login.
+
+Since telegram requires 2FA login and must be performed on the spot, there is no support for logging automatically given username and password.
+### Options
+
+Four environment variables make effects:
+
+- `TG_API_ID`: Required. The API ID. This should be a number.
+- `TG_API_HASH`: Required. The API hash.
+- `TG_SESSION_PATH`: Optional. The path where session token is saved.
+- `DESIRED_USERNAME`: Required. The username you want to take.
+
+If you do not own the API key, generate one [here](https://my.telegram.org/apps).
+
+### Exit Code
+
+- `0`: Successfully take the desired username.
+- `20`: Desired username is occupied.
+- `21`: Desired username is invalid.
+- `22`: Desired username already owned by you.
+- `30`: Failed to login.
+- `40`: Failed for flood.
+- `1`: Failed for any other reason.
+
+## Caution
+
+To avoid flood, run this script every 15 minutes would be good.
---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import asyncio
+import sys
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateUsernameRequest
+from telethon.errors import FloodWaitError, UsernameOccupiedError, UsernameInvalidError, UsernameNotModifiedError
+
+api_id = os.environ['TG_API_ID']
+api_hash = os.environ['TG_API_HASH']
+session_path = os.environ.get('TG_SESSION_PATH')
+desired_username = os.environ['DESIRED_USERNAME']
+
+# API ID should be number
+try:
+    api_id = int(api_id)
+except ValueError:
+    print('Invalid api id: ' + api_id)
+    sys.exit(30)
+
+def login():
+    # Login if needed
+    # https://docs.telethon.dev/en/stable/concepts/sessions.html#different-session-storage
+    session_text = ''
+    if session_path != None:
+        print('Use session file: ' + session_path)
+        try:
+            with open(session_path, 'r') as session_file:
+                session_text = session_file.read()
+        except:
+            print('Failed to read session. Try login.')
+    else:
+        print('No session file provided. Try login.')
+
+    if session_text == '' and not os.isatty(sys.stdout.fileno()):
+        # if no session is provided and this is not an interactive shell
+        # let the login failed without asking user.
+        print('Not interactive shell. Do not prompt login. Login failed.')
+        sys.exit(30)
+
+    try:
+        client = TelegramClient(StringSession(session_text), api_id, api_hash)
+    except ValueError as e:
+        print('Login failed.')
+        sys.exit(30)
+    return client
+
+
+async def task(client):
+    # https://docs.telethon.dev/en/stable/examples/users.html?highlight=updateusername#updating-your-username
+    try:
+        await client(UpdateUsernameRequest(desired_username))
+        print('Username updated: ' + desired_username)
+        return 0
+    except UsernameOccupiedError:
+        print('Username occupied: ' + desired_username)
+        return 20
+    except UsernameInvalidError:
+        print('Username invalid: ' + desired_username)
+        return 21
+    except UsernameNotModifiedError:
+        print('Username not modified: ' + desired_username)
+        return 22
+    except FloodWaitError as e:
+        # https://docs.telethon.dev/en/stable/quick-references/faq.html#how-can-i-except-floodwaiterror
+        print('Flood. Remaning: ' + str(e.seconds))
+        return 87
+
+
+def saveLogin(client):
+    if session_path != None:
+        print('Session path is set to save login: ' + session_path)
+        session_text = client.session.save()
+        with open(session_path, 'w') as session_file:
+            session_file.write(session_text)
+        print('Login saved.')
+
+
+async def main():
+    client = login()
+    async with client:
+        saveLogin(client)
+        exit_code = await task(client)
+        sys.exit(exit_code)
+
+asyncio.run(main())
